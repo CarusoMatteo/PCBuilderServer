99e43ddac83d1da9149b73ce4cb425aa1ea98ec1
CarusoMatteo <matteo.caruso.scuola@gmail.com>
2023-05-07 15:57:11 +0200

    Added the file to connect a component to a user

---
+++ AddToCart.php
@@ -0,0 +1,64 @@
+<?php
+
+$conn = require("SelectDatabase.php");
+
+$username = $_GET["Username"];
+$componentType = $_GET["ComponentType"];
+$componentID = $_GET["ComponentID"];
+
+switch ($componentType) {
+    case '0':
+        $attribute = "CPU";
+        break;
+    case '1':
+        $attribute = "Motherboard";
+        break;
+    case '2':
+        $attribute = "RAM";
+        break;
+    case '3':
+        $attribute = "GPU";
+        break;
+    case '4':
+        $attribute = "Storage";
+        break;
+    case '5':
+        $attribute = "PSU";
+        break;
+    default:
+        $attribute = "NotValid";
+        break;
+}
+
+$query =
+    "UPDATE user
+    SET $attribute = $componentID
+    WHERE Username = '$username'
+";
+
+// print($query . "<br>");
+
+$result = array("UpdateSuccessful" => true);
+
+try {
+    mysqli_query($conn, $query);
+} catch (Exception $e) {
+    // print($e);
+    $result["UpdateSuccessful"] = false;
+}
+
+mysqli_close($conn);
+
+$json = json_encode($result);
+print($json);
+
+// EXAMPLE:
+// AddToCart.php?Username="Admin"&ComponentType=0&ComponentID=null
+
+// QUERY:
+// UPDATE user 
+// SET CPU = null 
+// WHERE Username = "Admin"
+
+// RESULTING JSON:
+// {"UpdateSuccessful":true}
