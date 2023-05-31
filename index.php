6c1f346169d282760afad115bdad2547e7f094e5
CarusoMatteo <matteo.caruso.scuola@gmail.com>
2023-05-31 18:13:48 +0200

    Now the QR code contains just the code

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
+$fullURI = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+$code = substr($fullURI, 0, 18);
+print($code . "<br>");
 print('
-<a href="www.qr-code-generator.com/" border="0" style="cursor:default" rel="nofollow">
-<img src="https://chart.googleapis.com/chart?cht=qr&chl=http%3A%2F%2F' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . '&chs=180x180&choe=UTF-8&chld=L|2">
-</a>
+<img src="https://chart.googleapis.com/chart?cht=qr&chl=' . $code . '&chs=180x180&choe=UTF-8&chld=L|2" alt="qr code">
 ');
